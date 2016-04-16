362bc00751def8ee2aa03b9ea565dcbddfc1418a
Maksimkaa <Maksimkaa@tut.by>
2016-04-16 21:14:25 +0300

    Вычислить значение выражения по формуле x - x^3/3 + x^5/5

---
+++ Lesson01task01/src/by/maksimkaa/htp03/lesson01/Main.java
@@ -0,0 +1,14 @@
+package by.maksimkaa.htp03.lesson01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x=2;
+		
+		double y = x - Math.pow(x, 3);
+		
+		System.out.println(y);
+
+	}
+
+}
